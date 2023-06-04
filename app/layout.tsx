326864f1cd86b78a39524a189f97b4b2da29fbd8
jesky <jesky.yeremi@gmail.com>
2023-06-05 02:50:24 +0700

    @pokedex project init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Pokedex',
+  description: 'Modern pokedex app built on Next 13 and Typescript',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
