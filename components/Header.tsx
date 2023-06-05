6adf7528f3f2b39fea7d33b2fd9268a5543a29be
jesky <jesky.yeremi@gmail.com>
2023-06-06 06:26:12 +0700

    feature(home): created header component

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+// core components
+import Link from 'next/link';
+import Image from 'next/image';
+import { Box } from '@mui/material';
+
+// data
+import { Navigation } from '@/data/Navigations';
+
+export default function Header() {
+  return (
+    <Box
+      sx={{
+        height: '5rem',
+        width: '100%',
+        flexGrow: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        boxShadow:
+          '0 0.0625rem 0.5rem 0 rgba(0,0,0,.04), 0 0.0625rem 0.3125rem 0 rgba(0,0,0,.04)',
+      }}
+    >
+      <Box
+        sx={{
+          width: '22.5vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Link href='/'>
+          <Image
+            priority
+            src='/Pokemon.svg'
+            width={146}
+            height={54}
+            alt='Pokemon Logo'
+          />
+        </Link>
+        {Navigation.map((nav, index) => {
+          return (
+            <Link
+              key={index}
+              href={nav.url}
+              style={{
+                fontSize: '12px',
+                fontWeight: 500,
+                textTransform: 'capitalize',
+              }}
+            >
+              {nav.label}
+            </Link>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+}
---
+++ data/Navigations.ts
@@ -0,0 +1,14 @@
+export const Navigation = [
+  {
+    label: 'home',
+    url: '/',
+  },
+  {
+    label: 'games',
+    url: '/games',
+  },
+  {
+    label: 'items',
+    url: '/items',
+  },
+];
