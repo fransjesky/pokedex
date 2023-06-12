ffa502b9a1fb7a2e0a00ecdd8076b90329baca6e
jesky <jesky.yeremi@gmail.com>
2023-06-13 06:11:00 +0700

    feat(card): added chart.js and react-chartjs-2 dependency for card base stats chart ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import { Montserrat } from 'next/font/google';
 
 // components
-import Head from 'next/head';
 import Header from '@/components/Header';
 
 const montserrat = Montserrat({
@@ -24,13 +23,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <Head>
-        <link
-          rel='preload'
-          href='/_next/static/css/app/layout.css?v=1686225477758'
-          as='style'
-        />
-      </Head>
       <body className={montserrat.className}>
         <Header />
         <main style={{ marginTop: '5rem' }}>{children}</main>
